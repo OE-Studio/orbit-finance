71dd975cbeec7e1d353ec12194bbf86a9f92d4db
david660 <daodu.muyiwa@gmail.com>
2023-10-27 21:32:16 +0100

    Fixed corrections

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -42,7 +42,6 @@ export default function Input({placeholder, onChange, name, type, value}) {
     }
 
     useEffect(()=>{
-        // spanRef.current.innerHTML = "Please enter a valid email"
         if(type  &&  type=== "email" && value){
             if(value  && spanRef && !value.includes("@")) {
                 setShowError(true)
